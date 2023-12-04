1a469483001b3942c20bd690640e135a69c4a08f
junkaM <hei.mikanto.3@gmail.com>
2023-12-04 19:19:27 +0100

    fix: fix little conflits in main

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,7 @@ import repository.CrudOperations;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-<<<<<<< HEAD
-
-=======
         AccountCrudOperations accountCrudOperations = new AccountCrudOperations();
         accountCrudOperations.findAll();
->>>>>>> 7553b3de8228ac4d8ffd6596cb0ec6ca05cf6ddc
     }
 }
