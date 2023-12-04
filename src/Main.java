7553b3de8228ac4d8ffd6596cb0ec6ca05cf6ddc
Zo-Tsilavina <hei.tsilavina@gmail.com>
2023-12-04 20:35:28 +0300

    feat: add test for findAll of accountCrudOperation in main

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
+import repository.AccountCrudOperations;
+import repository.CrudOperations;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-
-        }
+        AccountCrudOperations accountCrudOperations = new AccountCrudOperations();
+        accountCrudOperations.findAll();
     }
 }
