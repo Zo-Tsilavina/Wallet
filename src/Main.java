a1ecb65b3c0adbedcfcf01b26a686700313b5693
junkaM <hei.mikanto.3@gmail.com>
2023-12-07 13:59:59 +0100

    fix: account model and account sql script

--- src/Main.java
+++ src/Main.java
@@ -3,20 +3,19 @@ import models.Account;
 
 import java.sql.Timestamp;
 
-
 public class Main {
     public static void main(String[] args) {
         AccountCrudOperations accountCrudOperations = new AccountCrudOperations();
-//        Account test = new Account(
-//                3,
-//                "BOA",
-//                10000.00,
-//                Timestamp.valueOf("2025-05-05 08:30:00"),
-//                1,
-//                1,
-//                "espece"
-//        );
-//        System.out.println(accountCrudOperations.save(test));
+        Account test = new Account(
+                3,
+                "BOA",
+                10000.00,
+                Timestamp.valueOf("2025-05-05 08:30:00"),
+                1,
+                1,
+                "espece"
+        );
+        System.out.println(accountCrudOperations.save(test));
         System.out.println(accountCrudOperations.findAll());
     }
 }
