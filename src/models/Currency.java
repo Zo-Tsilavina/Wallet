73df71ebd8d765a02db9c801e976d1453ddff2eb
Zo-Tsilavina <hei.tsilavina@gmail.com>
2023-12-07 11:19:51 +0300

    fix: change the name of the column in the script sql and in model, CURRENCY

--- src/models/Currency.java
+++ src/models/Currency.java
@@ -5,28 +5,12 @@ import java.util.Objects;
 public class Currency {
     private int id;
     private String name;
-    private String symbol;
+    private String code;
 
-    @Override
-    public String toString() {
-        return "Currency{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", symbol='" + symbol + '\'' +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Currency currency = (Currency) o;
-        return id == currency.id && Objects.equals(name, currency.name) && Objects.equals(symbol, currency.symbol);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, symbol);
+    public Currency(int id, String name, String code) {
+        this.id = id;
+        this.name = name;
+        this.code = code;
     }
 
     public int getId() {
@@ -45,17 +29,33 @@ public class Currency {
         this.name = name;
     }
 
-    public String getSymbol() {
-        return symbol;
+    public String getCode() {
+        return code;
     }
 
-    public void setSymbol(String symbol) {
-        this.symbol = symbol;
+    public void setCode(String code) {
+        this.code = code;
     }
 
-    public Currency(int id, String name, String symbol) {
-        this.id = id;
-        this.name = name;
-        this.symbol = symbol;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Currency currency = (Currency) o;
+        return id == currency.id && Objects.equals(name, currency.name) && Objects.equals(code, currency.code);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, code);
+    }
+
+    @Override
+    public String toString() {
+        return "Currency{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", code='" + code + '\'' +
+                '}';
     }
 }
