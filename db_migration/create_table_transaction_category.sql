e600f55b66b505068d53c883883608a12594dafc
junkaM <hei.mikanto.3@gmail.com>
2023-12-15 06:42:19 +0100

    add script sql for transaction category

--- db_migration/create_table_transaction_category.sql
+++ db_migration/create_table_transaction_category.sql
@@ -4,3 +4,14 @@ CREATE TABLE transaction_categories(
      name VARCHAR(50),
      type VARCHAR(50)
 );
+
+INSERT INTO transaction_categories (transaction_category_id, name, type) VALUES (1, 'Nourritures & boissons', 'debit');
+INSERT INTO transaction_categories (transaction_category_id, name, type) VALUES (2, 'Achats', 'debit');
+INSERT INTO transaction_categories (transaction_category_id, name, type) VALUES (3, 'Logement', 'debit');
+INSERT INTO transaction_categories (transaction_category_id, name, type) VALUES (4, 'Transport', 'debit');
+INSERT INTO transaction_categories (transaction_category_id, name, type) VALUES (5, 'Vehicule', 'debit');
+INSERT INTO transaction_categories (transaction_category_id, name, type) VALUES (6, 'Loisirs', 'debit');
+INSERT INTO transaction_categories (transaction_category_id, name, type) VALUES (7, 'Multimedia, PC', 'debit');
+INSERT INTO transaction_categories (transaction_category_id, name, type) VALUES (8, 'Frais financiers', 'debit');
+INSERT INTO transaction_categories (transaction_category_id, name, type) VALUES (9, 'Investissement', 'debit');
+INSERT INTO transaction_categories (transaction_category_id, name, type) VALUES (10, 'Revenu', 'credit');
